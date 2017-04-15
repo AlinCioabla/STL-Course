49c627657295775de1614a09b06d18d42acaa538
AlinCioabla <kriogenik1@gmail.com>
2017-04-15 12:51:49 +0300

    Updated Projects

---
+++ Snake STL Refactor/Original/stdafx.h
@@ -0,0 +1,21 @@
+
+#pragma once
+
+#include "targetver.h"
+
+#include <stdio.h>
+#include <tchar.h>
+#include <iterator>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <windows.h>
+
+#include <iomanip>
+#include <random>
+#include <cmath>
+#include <set>
+
+
+using namespace std;
